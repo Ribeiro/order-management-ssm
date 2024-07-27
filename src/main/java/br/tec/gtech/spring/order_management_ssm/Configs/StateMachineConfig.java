247728c4420741933b2d25e11e72cd8a88b9c494
Geovanny Silva Ribeiro <geovannysilvaribeiro@MacBook-Air-de-Geovanny.local>
2024-07-27 07:59:18 -0300

    feat: create StateMachineFactory

---
+++ src/main/java/br/tec/gtech/spring/order_management_ssm/Configs/StateMachineConfig.java
@@ -0,0 +1,15 @@
+package br.tec.gtech.spring.order_management_ssm.Configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
+
+@Configuration
+@EnableStateMachineFactory
+public class StateMachineConfig extends EnumStateMachineConfigurerAdapter{
+
+
+
+
+    
+}
