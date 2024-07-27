8d3b8da7751f6ec22743358118af8c67da6e605c
Geovanny Silva Ribeiro <geovannysilvaribeiro@MacBook-Air-de-Geovanny.local>
2024-07-27 08:28:58 -0300

    feat: configured machine actions

--- src/main/java/br/tec/gtech/spring/order_management_ssm/StateMachine/StateMachineConfig.java
+++ src/main/java/br/tec/gtech/spring/order_management_ssm/StateMachine/StateMachineConfig.java
@@ -2,7 +2,9 @@ package br.tec.gtech.spring.order_management_ssm.StateMachine;
 
 import java.util.EnumSet;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
@@ -10,35 +12,56 @@ import org.springframework.statemachine.config.builders.StateMachineTransitionCo
 
 @Configuration
 @EnableStateMachineFactory
-public class StateMachineConfig extends EnumStateMachineConfigurerAdapter<OrderStates, OrderEvents>{
+public class StateMachineConfig extends EnumStateMachineConfigurerAdapter<OrderStates, OrderEvents> {
 
-@Override
-public void configure(StateMachineStateConfigurer<OrderStates, OrderEvents> states) throws Exception {
-    states.withStates()
-    .initial(OrderStates.NEW)
-    .states(EnumSet.allOf(OrderStates.class))
-    .end(OrderStates.COMPLETED)
-    .end(OrderStates.CANCELLED);
-}
+    @Override
+    public void configure(StateMachineStateConfigurer<OrderStates, OrderEvents> states) throws Exception {
+        states.withStates()
+                .initial(OrderStates.NEW)
+                .states(EnumSet.allOf(OrderStates.class))
+                .end(OrderStates.COMPLETED)
+                .end(OrderStates.CANCELLED);
+    }
 
-@Override
-public void configure(StateMachineTransitionConfigurer<OrderStates, OrderEvents> transitions) throws Exception {
-    transitions
-    .withExternal().source(OrderStates.NEW).target(OrderStates.VALIDATED).event(OrderEvents.VALIDATE)   
-    .and()
-    .withExternal().source(OrderStates.VALIDATED).target(OrderStates.PAID).event(OrderEvents.PAY)   
-    .and()
-    .withExternal().source(OrderStates.PAID).target(OrderStates.SHIPPED).event(OrderEvents.SHIP)
-    .and()
-    .withExternal().source(OrderStates.SHIPPED).target(OrderStates.COMPLETED).event(OrderEvents.COMPLETE)
-    .and()
-    .withExternal().source(OrderStates.VALIDATED).target(OrderStates.CANCELLED).event(OrderEvents.CANCEL)
-    .and()
-    .withExternal().source(OrderStates.PAID).target(OrderStates.CANCELLED).event(OrderEvents.CANCEL);
+    @Override
+    public void configure(StateMachineTransitionConfigurer<OrderStates, OrderEvents> transitions) throws Exception {
+        transitions
+                .withExternal().source(OrderStates.NEW).target(OrderStates.VALIDATED).event(OrderEvents.VALIDATE)
+                .action(validateOrderAction())
+                .and()
+                .withExternal().source(OrderStates.VALIDATED).target(OrderStates.PAID).event(OrderEvents.PAY)
+                .action(payOrderAction())
+                .and()
+                .withExternal().source(OrderStates.PAID).target(OrderStates.SHIPPED).event(OrderEvents.SHIP)
+                .action(shipOrderAction())
+                .and()
+                .withExternal().source(OrderStates.SHIPPED).target(OrderStates.COMPLETED).event(OrderEvents.COMPLETE)
+                .and()
+                .withExternal().source(OrderStates.VALIDATED).target(OrderStates.CANCELLED).event(OrderEvents.CANCEL)
+                .and()
+                .withExternal().source(OrderStates.PAID).target(OrderStates.CANCELLED).event(OrderEvents.CANCEL);
 
-    
-}
+    }
 
+    @Bean
+    Action<OrderStates, OrderEvents> shipOrderAction() {
+        return context -> {
+            System.out.println("Shipping order...");
+        };
+    }
 
-    
+    @Bean
+    Action<OrderStates, OrderEvents> payOrderAction() {
+        return context -> {
+            System.out.println("Paying order...");
+        };
+    }
+
+    @Bean
+    Action<OrderStates, OrderEvents> validateOrderAction() {
+        return context -> {
+            System.out.println("Validating order...");
+        };
+    }
+
 }
