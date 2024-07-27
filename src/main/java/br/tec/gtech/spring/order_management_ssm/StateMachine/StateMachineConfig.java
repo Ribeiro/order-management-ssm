89c7d2c88f43e72f83b5bfb62b7d0596e2794138
Geovanny Silva Ribeiro <geovannysilvaribeiro@MacBook-Air-de-Geovanny.local>
2024-07-27 08:21:04 -0300

    feat: configured machine transitions

--- src/main/java/br/tec/gtech/spring/order_management_ssm/StateMachine/StateMachineConfig.java
+++ src/main/java/br/tec/gtech/spring/order_management_ssm/StateMachine/StateMachineConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 @Configuration
 @EnableStateMachineFactory
@@ -18,6 +19,23 @@ public void configure(StateMachineStateConfigurer<OrderStates, OrderEvents> stat
     .states(EnumSet.allOf(OrderStates.class))
     .end(OrderStates.COMPLETED)
     .end(OrderStates.CANCELLED);
+}
+
+@Override
+public void configure(StateMachineTransitionConfigurer<OrderStates, OrderEvents> transitions) throws Exception {
+    transitions
+    .withExternal().source(OrderStates.NEW).target(OrderStates.VALIDATED).event(OrderEvents.VALIDATE)   
+    .and()
+    .withExternal().source(OrderStates.VALIDATED).target(OrderStates.PAID).event(OrderEvents.PAY)   
+    .and()
+    .withExternal().source(OrderStates.PAID).target(OrderStates.SHIPPED).event(OrderEvents.SHIP)
+    .and()
+    .withExternal().source(OrderStates.SHIPPED).target(OrderStates.COMPLETED).event(OrderEvents.COMPLETE)
+    .and()
+    .withExternal().source(OrderStates.VALIDATED).target(OrderStates.CANCELLED).event(OrderEvents.CANCEL)
+    .and()
+    .withExternal().source(OrderStates.PAID).target(OrderStates.CANCELLED).event(OrderEvents.CANCEL);
+
     
 }
 
